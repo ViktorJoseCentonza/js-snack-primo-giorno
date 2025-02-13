33e91ca188a62bb1f6be4b97e610beafabf5bf55
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-13 10:46:09 +0100

    add extra check for Nan

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
 
-const first_number = Number(prompt("Scrivi il primo numero"))
-const second_number = Number(prompt("Scrivi il secondo numero"))
+const first_number = Number(prompt("Scrivi il primo numero (!!usa il punto per i numeri decimali!!)"))
+const second_number = Number(prompt("Scrivi il secondo numero (!!usa il punto per i numeri decimali!!)"))
 
 if (first_number > second_number) {
     console.log(`${first_number} è maggiore di ${second_number}`);
 } else if (second_number > first_number) {
     console.log(`${second_number} è maggiore di ${first_number}`);
+} else if (first_number == NaN || second_number == NaN) {
+    console.log(`Errore in input! attenzione ai valori forniti!`);
 } else {
     console.log(`il maggiore è nessuno, sono uguali! primo:${first_number}, secondo:${second_number}.`);
 }  
