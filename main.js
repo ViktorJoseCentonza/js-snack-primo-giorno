e9f25c6ab25d37beb8a0d275c0993ec8de0c1529
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-13 10:42:48 +0100

    edit parseInt to Number for input, remove variable log, edit result logs

--- main.js
+++ main.js
@@ -3,16 +3,13 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
 
-const first_number = parseInt(prompt("Scrivi il primo numero"))
-const second_number = parseInt(prompt("Scrivi il secondo numero"))
-parseInt()
-console.log(first_number);
-console.log(second_number);
+const first_number = Number(prompt("Scrivi il primo numero"))
+const second_number = Number(prompt("Scrivi il secondo numero"))
 
 if (first_number > second_number) {
-    console.log(`il maggiore è ${first_number}`);
+    console.log(`${first_number} è maggiore di ${second_number}`);
 } else if (second_number > first_number) {
-    console.log(`il maggiore è ${second_number}`);
+    console.log(`${second_number} è maggiore di ${first_number}`);
 } else {
-    console.log(`il maggiore è nessuno, sono uguali! primo:${first_number}, secondo:${second_number}`);
+    console.log(`il maggiore è nessuno, sono uguali! primo:${first_number}, secondo:${second_number}.`);
 }  
