7c622ea200c0f77c17eed8e0e6b299740565ded4
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-13 10:32:36 +0100

    first snack

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
 
+const first_number = prompt("Scrivi il primo numero")
+const second_number = prompt("Scrivi il secondo numero")
+
+if (first_number > second_number) {
+    console.log(`il maggiore è ${first_number}`);
+} else if (first_number < second_number) {
+    console.log(`il maggiore è ${second_number}`);
+} else {
+    console.log(`il maggiore è nessuno, sono uguali! primo:${first_number}, secondo:${second_number}`);
+}  
